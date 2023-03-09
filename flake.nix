ea53181f52fffbd45cda988c54215d76b0960576
John Lord <john@lordsonline.org>
2023-03-09 16:09:53 -0600

    Added nixos-hardware and xps-9300

--- flake.nix
+++ flake.nix
@@ -5,14 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { home-manager, nixpkgs, ... }: {
+  outputs = { home-manager, nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations = {
       pluto = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/pluto/configuration.nix
+          nixos-hardware.nixosModules.dell-xps-13-9300
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
