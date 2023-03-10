dd7af854e0af384e01a885f0c04a19b57db1893d
John Lord <john@lordsonline.org>
2023-03-10 15:08:24 -0600

    Completed printer setup

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -90,6 +90,9 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.openFirewall = true;
 
   # Enable sound.
   sound.enable = true;
