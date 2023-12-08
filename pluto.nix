a3a47f28fa50d08db269b90c8b7c37fa0c35bcfc
John Lord <john@lordsonline.org>
2023-12-08 07:48:35 -0600

    Patched libnvme and libblockdev using overlays

--- pluto.nix
+++ pluto.nix
@@ -1,6 +1,33 @@
 { config, pkgs, inputs, lib, modulesPath, ... }:
 
 {
+  nixpkgs.overlays = [
+    ( final: prev:
+      {
+        libnvme = prev.libnvme.overrideAttrs (old: {
+          patches = (old.patches or []) ++ [
+            (prev.fetchpatch {
+              url = "https://github.com/linux-nvme/libnvme/pull/727.patch";
+              hash = "sha256:0SrR1++QTXNq+X4YOpW3JzEjFdSSBdqPl/xI2zdcvTs=";
+            })
+          ];
+        });
+      } )
+    ( final: prev:
+      {
+        libblockdev = prev.libblockdev.overrideAttrs (old: {
+          patches = (old.patches or []) ++ [
+            (prev.fetchpatch {
+              url = "https://github.com/storaged-project/libblockdev/pull/969.patch";
+              hash = "sha256:LHeotKzcRDdT/GhH3JdVjX/7ZMN1ghllYuaxPYsCZMY=";
+            })
+          ];
+        });
+      } )
+  ];
+
+
+
   # Use the grub EFI boot loader due to HiDPI screen
   boot = {
     loader = {
