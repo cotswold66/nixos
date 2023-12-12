b6ddd65c21ad5379b3866a4747cbea7f82383ad5
John Lord <john@lordsonline.org>
2023-12-12 05:31:10 -0600

    Added notmuch to emacs packages

--- home/editors/emacs.nix
+++ home/editors/emacs.nix
@@ -1,30 +1,23 @@
-{ config, pkgs,  ...}
-  
-  let 
-    my-emacs = pkgs.emacs29-pgtk;
-  in
-    {
-      programs.emacs = {
-        enable = true;
-        package = my-emacs;
-        extraPackages = [
-          mu4e
-        ];
-};
-      
-      services.emacs = {
-        enable = true;
-        client.enable = true;
-        defaultEditor= true;
-        package = my-emacs;
-      };
+{ config, pkgs,  ...}:
 
-      home.packages = with pkgs; [
-        fd
-        gcc
-        ghostscript
-        ripgrep
-        source-code-pro
-      ];
-    };
-};
+let 
+  my-emacs = ((pkgs.emacsPackagesFor pkgs.emacs29-pgtk).emacsWithPackages (
+    epkgs: [ epkgs.notmuch ])
+  );
+in
+{  
+  services.emacs = {
+    enable = true;
+    client.enable = true;
+    defaultEditor= true;
+    package = my-emacs;
+  };
+
+  home.packages = with pkgs; [
+    fd
+    gcc
+    ghostscript
+    ripgrep
+    source-code-pro
+  ];
+}
