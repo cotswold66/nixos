e889d40b9bde07ee2c7fe29310a333fde86d6f58
John Lord <john@lordsonline.org>
2023-12-12 05:19:16 -0600

    Removed not needed modules

--- kde.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, inputs, lib, modulesPath, ... }:
-
-{
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
-}
