61b1e2c033131783f854ab15f847cdf3373b98e2
John Lord <john@lordsonline.org>
2023-12-17 10:14:25 -0600

    Update flake.lock and remove libnvme overlay since fixed upstream

--- hosts/pluto/default.nix
+++ hosts/pluto/default.nix
@@ -2,17 +2,17 @@
 
 {
   nixpkgs.overlays = [
-    ( final: prev:
-      {
-        libnvme = prev.libnvme.overrideAttrs (old: {
-          patches = (old.patches or []) ++ [
-            (prev.fetchpatch {
-              url = "https://github.com/linux-nvme/libnvme/pull/727.patch";
-              hash = "sha256:0SrR1++QTXNq+X4YOpW3JzEjFdSSBdqPl/xI2zdcvTs=";
-            })
-          ];
-        });
-      } )
+    # ( final: prev:
+    #   {
+    #     libnvme = prev.libnvme.overrideAttrs (old: {
+    #       patches = (old.patches or []) ++ [
+    #         (prev.fetchpatch {
+    #           url = "https://github.com/linux-nvme/libnvme/pull/727.patch";
+    #           hash = "sha256:0SrR1++QTXNq+X4YOpW3JzEjFdSSBdqPl/xI2zdcvTs=";
+    #         })
+    #       ];
+    #     });
+    #   } )
     ( final: prev:
       {
         libblockdev = prev.libblockdev.overrideAttrs (old: {
